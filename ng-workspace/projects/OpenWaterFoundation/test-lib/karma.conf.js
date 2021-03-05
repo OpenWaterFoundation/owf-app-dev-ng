77fc870d9de0fb69941f8028a20a0d32c7a9dc03
Nightsphere <nightsphere88@gmail.com>
2021-03-05 11:27:59 -0700

    Changed scope to fit owner name

--- ng-workspace/projects/OpenWaterFoundation/test-lib/karma.conf.js
+++ ng-workspace/projects/OpenWaterFoundation/test-lib/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/open-water-foundation/test-lib'),
+      dir: require('path').join(__dirname, '../../../coverage/OpenWaterFoundation/test-lib'),
       subdir: '.',
       reporters: [
         { type: 'html' },
