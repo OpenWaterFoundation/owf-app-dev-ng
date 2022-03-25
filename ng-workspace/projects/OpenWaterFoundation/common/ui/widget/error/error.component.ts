05b5529283c4dad72452d1a88f0b27e93eb5f18c
Nightsphere <nightsphere88@gmail.com>
2022-03-25 17:46:27 -0600

    Updated dashboard widgets.

--- ng-workspace/projects/OpenWaterFoundation/common/ui/widget/error/error.component.ts
+++ ng-workspace/projects/OpenWaterFoundation/common/ui/widget/error/error.component.ts
@@ -13,22 +13,25 @@ import { WidgetService }    from '../widget.service';
 })
 export class ErrorComponent {
 
+  /** A string provided as input to this widget template tag that describes
+   * what error occurred. */
   @Input('errorType') errorType: string;
-
+  /** An InfoMapper widget type to describe the widget where the error
+   * took place. */
   @Input('name') errorWidgetName: IM.Widget;
-
-  readonly supportedImageFiles = [
+  /** The list of supported image widget files. */
+  readonly supportedImageFiles: string[] = [
     'jpg',
     'png'
   ];
-
-  readonly supportedTextFiles = [
+  /** The list of supported text widget files. */
+  readonly supportedTextFiles: string[] = [
     'html',
     'md'
   ];
 
   /**
-   * 
+   * @constructor The Error widget component constructor.
    * @param owfCommonService The injected Common library service.
    * @param widgetService The inject local Widget service.
    */
@@ -37,7 +40,7 @@ export class ErrorComponent {
 
 
   /**
-   * 
+   * Determine what Image widget error occurred.
    */
   imageError(): void {
     switch(this.errorType) {
@@ -53,12 +56,12 @@ export class ErrorComponent {
     // Determine which widget is erroring.
     switch(this.errorWidgetName) {
       case IM.Widget.img: this.imageError(); break;
-      case IM.Widget.text: 
+      case IM.Widget.text: this.textError(); break;
     }
   }
 
   /**
-   * 
+   * Universal error message for any widget not given a `dataPath` property.
    */
   noDataPath(): void {
     console.error('No "dataPath" property found for the "' + this.errorWidgetName +
@@ -66,6 +69,9 @@ export class ErrorComponent {
     ' widget has been provided and is correct.');
   }
 
+  /**
+   * Determine what Text widget error occurred.
+   */
   textError(): void {
     switch(this.errorType) {
       case 'no dataPath': this.noDataPath(); break;
@@ -74,12 +80,22 @@ export class ErrorComponent {
   }
 
   /**
-   * 
+   * Error message if an unsupported file is provided in the dashboard configuration
+   * file, and lists the currently supported files.
    */
   unsupportedFile(): void {
+
+    var supportedFiles: string[];
+
+    switch(this.errorWidgetName) {
+      case IM.Widget.img:
+        supportedFiles = this.supportedImageFiles; break;
+      case IM.Widget.text:
+        supportedFiles = this.supportedTextFiles; break;
+    }
     
-    console.error('"' + this.errorWidgetName + '" data files must be one of the following' +
-    ' supported types: ');
+    console.error('"' + this.errorWidgetName + '" widget data files must be one ' +
+    'of the following supported types: [' + supportedFiles + ']');
   }
 
 }
--- ng-workspace/projects/OpenWaterFoundation/common/ui/widget/status-indicator/status-indicator.component.ts
+++ ng-workspace/projects/OpenWaterFoundation/common/ui/widget/status-indicator/status-indicator.component.ts
@@ -12,7 +12,26 @@ import * as IM              from '@OpenWaterFoundation/common/services';
 })
 export class StatusIndicatorComponent {
   
-
+  /** Displays a red down caret icon in the widget if set to true. */
+  changeDecBad: boolean;
+  /** Displays a green down caret icon in the widget if set to true. */
+  changeDecGood: boolean;
+  /** Displays a red up caret icon in the widget if set to true. */
+  changeIncBad: boolean;
+  /** Displays a green up caret icon in the widget if set to true. */
+  changeIncGood: boolean;
+  /** Displays a red X icon in the widget if set to true. */
+  failureIndicator: boolean;
+  /** Displays a green check icon in the widget if set to true. */
+  passingIndicator: boolean;
+  /** The title of this widget from the widget's `title` property in the dashboard
+   * configuration file. */
+  @Input() title: string;
+  /** Displays a yellow exclamation icon in the widget if set to true. */
+  warningIndicator: boolean;
+  /**
+   * 
+   */
   widgetError: boolean;
 
 
@@ -27,8 +46,8 @@ export class StatusIndicatorComponent {
    * Called right after the constructor.
    */
   ngOnInit(): void {
-
-    
+    // HARD CODE the displaying 'main' and 'change' indicator icons for now.
+    this.passingIndicator = true; this.changeDecBad = true;
   }
 
   
