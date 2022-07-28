9892237f91b4afef4d64eefb230d117adeb83f5c
Nightsphere <nightsphere88@gmail.com>
2022-07-27 23:09:12 -0600

    Created the not-found component to be displayed by the Common package routing.

--- ng-workspace/projects/OpenWaterFoundation/common/services/public-api.ts
+++ ng-workspace/projects/OpenWaterFoundation/common/services/public-api.ts
@@ -1,9 +1,15 @@
 // ERROR component.
 export * from './error/error.component';
 export * from './error/error.module';
+// NOT FOUND component.
+export * from './not-found/not-found.component';
+export * from './not-found/not-found.module';
 
+// EVENT service.
 export * from './event.service';
+// Common service.
 export * from './owf-common.service';
-
+// All library types.
 export * from './types';
+// All library static data structures & variables.
 export * from './static-structures';
